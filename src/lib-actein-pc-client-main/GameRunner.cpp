34b592a4f157816ab1dcbf19a4582790302da11c
Artem Brazhnikov <artem.bv@protonmail.com>
2016-11-17 17:55:43 +0100

    when steam vr opened: steam does not start a game. That's why we need to log in to steam and then run a game. I added Steam API logic, but now we are running only service under LocalSystem account. To work Steam API client must be run under the same windows account as Steam. So now we just wait for a while after steam.exe run and start a game

--- src/lib-actein-pc-client-main/GameRunner.cpp
+++ src/lib-actein-pc-client-main/GameRunner.cpp
@@ -3,6 +3,7 @@
 #include <gen/vr_game.pb.h>
 #include <StringUtils.h>
 #include <WinServiceUtils.h>
+//#include "SteamClient.h"
 #include "Settings.h"
 #include "GameRunner.h"
 
@@ -19,14 +20,30 @@ namespace actein
     {
         std::unique_lock<std::mutex> locker(mSync);
 
-        std::wstringstream steamCommanLine;
-        steamCommanLine << utils::string2wstring(mSettings.GetSteamExePath())
-            << L" -applaunch " << std::to_wstring(game.steam_game_id()) << " -login "
-            << utils::string2wstring(mSettings.GetSteamAccountName()) << " "
-            << utils::string2wstring(mSettings.GetSteamAccountPassword());
+        std::wostringstream steamLoginCmdLine;
+        steamLoginCmdLine << utils::string2wstring(mSettings.GetSteamExePath())
+            << " -login " << utils::string2wstring(mSettings.GetSteamAccountName())
+            << " " << utils::string2wstring(mSettings.GetSteamAccountPassword());
 
-        utils::RunInteractiveProcess(steamCommanLine.str());
+        utils::RunInteractiveProcess(steamLoginCmdLine.str());
+
+        // TODO: wait until Steam logs in; when we will have ui application:
+        // use Steam API to check if user is logged in to Steam
+        ::Sleep(10000);
+        /*SteamClient steamClient;
+        if (steamClient.WaitForSteamUserToLogin(std::chrono::milliseconds(15000)))
+        {
+            throw std::runtime_error("Can not login to the Steam");
+        }
+        steamClient.Shutdown();*/
+
+        std::wostringstream steamRunGameCmdLine;
+        steamRunGameCmdLine << utils::string2wstring(mSettings.GetSteamExePath())
+            << L" steam://launch/" << std::to_wstring(game.steam_game_id()) << L"/VR";
         
+        utils::RunInteractiveProcess(steamRunGameCmdLine.str());
+        
+        // TODO: this feature block headset's screen also, so not it's disabled
         //std::wstring runDllLockScreen = L"rundll32.exe user32.dll,LockWorkStation";
         //utils::RunInteractiveProcess(runDllLockScreen);
 
