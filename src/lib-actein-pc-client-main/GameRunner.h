06e0d88001146453db4c939d245b460fdb96088f
Artem Brazhnikov <artem.bv@protonmail.com>
2016-12-05 11:05:40 +0100

    expand time interval of steam vr start

--- src/lib-actein-pc-client-main/GameRunner.h
+++ src/lib-actein-pc-client-main/GameRunner.h
@@ -39,7 +39,7 @@ namespace actein
         std::shared_ptr<spdlog::logger> mLogger;
 
         static const int STEAM_VR_ID = 250820;
-        static const int MAX_START_STEAM_VR_WAIT_TIME = 12000;
+        static const int MAX_START_STEAM_VR_WAIT_TIME = 15000;
         static const int WAIT_STEP = 1000;
         static const int STEAM_VR_STARTUP_WAIT_TIME = 6000;
     };
